edf3a46bd70fb529bb4c681ce231fa13817bd2e1
Vitaliy <vito.co@ya.ru>
2024-05-06 21:57:18 +0300

    - friend command

---
+++ app/Telegram/Commands/FriendCommand.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace App\Telegram\Commands;
+
+use App\Models\Player;
+use Illuminate\Support\Facades\Log;
+use Telegram\Bot\Commands\Command;
+use Telegram\Bot\Objects\Message;
+
+class FriendCommand extends Command
+{
+	protected string $name = 'friend';
+	protected array $aliases = ['referrer'];
+	protected string $description = 'Invite Friends Command';
+
+	public function handle()
+	{
+		$message = $this->getUpdate()->getMessage();
+		if (!$message instanceof Message) {
+			Log::debug('instanceof', [$message instanceof Message, $message]);
+			return;
+		}
+		$chatId = $message->getChat()->id;
+		$url = 'https://t.me/dev_kombat_bot?start=' . $chatId;
+
+		$text = 'Share *DevKombat* with your firends and earn bonuses for each friend you invite and for their activity.' . PHP_EOL . PHP_EOL;
+		$text .= 'Copy and share it' . PHP_EOL;
+		$text .= '👉 `' . $url . '`';
+
+		$this->replyWithMessage([
+			'parse_mode' => 'MarkDown',
+			'text' => $text,
+		]);
+	}
+}
