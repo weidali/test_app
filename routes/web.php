2b6206b2ec9df8d6d5aec8b7e6ddf5415e05d6f0
Vitaliy <vito.co@ya.ru>
2024-05-06 12:58:25 +0300

    - install logger

--- routes/web.php
+++ routes/web.php
@@ -10,3 +10,4 @@ Route::get('/', function () {
 Route::post('/<token>/webhook', [TelegramBotController::class, 'handleWebhook']);
 
 Route::get('/show', [TelegramBotController::class, 'show']);
+Route::get('admin/logs', [\Rap2hpoutre\LaravelLogViewer\LogViewerController::class, 'index']);
