e0a09d1147ffdb876c6b1411bea88af68cb1c54c
Vitaliy <vito.co@ya.ru>
2024-05-08 20:20:45 +0300

    - command: refresh players bio

---
+++ app/Console/Commands/RefreshPlayersBio.php
@@ -0,0 +1,85 @@
+<?php
+
+namespace App\Console\Commands;
+
+use App\Models\Player;
+use Illuminate\Console\Command;
+use Illuminate\Support\Facades\Log;
+use Telegram\Bot\Exceptions\TelegramResponseException;
+use Telegram\Bot\Laravel\Facades\Telegram;
+
+class RefreshPlayersBio extends Command
+{
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'app:refresh-players-bio';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'Command description';
+
+    /**
+     * Execute the console command.
+     */
+    public function handle()
+    {
+        $first_name = null;
+        $last_name = null;
+        $file_id = null;
+
+        $players = Player::select('chat_id')->get();
+        foreach ($players as $key => $player) {
+            try {
+                $photos = Telegram::getUserProfilePhotos([
+                    'user_id' => $player->chat_id,
+                ]);
+                $file_id = $photos['photos'][0][0]['file_id'];
+                dump($file_id);
+            } catch (TelegramResponseException $e) {
+                Log::error('[RefreshPlayersBioCredentials] getUserProfilePhotos', [
+                    'error' => $e->getMessage(),
+                ]);
+            }
+
+            try {
+                $chat = Telegram::getChat([
+                    'user_id' => $player->chat_id,
+                    'chat_id' => $player->chat_id,
+                ]);
+
+                $first_name = $chat['first_name'];
+                if (isset($chat['last_name'])) {
+                    $last_name = $chat['last_name'];
+                }
+                $file_id = $chat['photo']['small_file_id'];
+                // $photo = $chat['photo']['small_file_id'];
+                dump($file_id);
+            } catch (TelegramResponseException $e) {
+                Log::error('[RefreshPlayersBioCredentials] getChat', [
+                    'error' => $e->getMessage(),
+                ]);
+            }
+
+            if (!is_null($first_name)) {
+                $player->setAttribute('first_name', $first_name);
+            }
+            if (!is_null($last_name)) {
+                $player->setAttribute('last_name', $last_name);
+            }
+            if (!is_null($file_id)) {
+                $player->setAttribute('file_id', $file_id);
+            }
+            $player->save();
+        }
+
+        Log::info('[CalculatePlayersRatingByBalance]', [
+            'count of rated players' => $player->count(),
+        ]);
+    }
+}
